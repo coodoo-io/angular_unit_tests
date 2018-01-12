ecab19a9db4d466bc8e5172a1ce5ffe74b2f4cb0
Marcel Ploch <marcel.ploch@coodoo.de>
2018-01-12 21:19:18 +0100

    Content after Part One

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,96 @@
+'use strict';
+exports.__esModule = true;
+var path = require('path');
+var webpack = require('webpack');
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function (config) {
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+var coverage = process.env.COVERAGE || false;
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    basePath: './',
+    frameworks: ['jasmine'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-jasmine'), require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-phantomjs-launcher'), require('karma-story-reporter'),
+      require('karma-webpack'), require('karma-sourcemap-loader')
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    client: {
+      clearContext:
+          false  // leave Jasmine Spec Runner output visible in browser
     },
+    files: [
+      {pattern: './src/test.ts', watched: false},
+      {
+        pattern: './src/**/*.html',
+        included: false,
+        watched: true,
+        served: true
+      },
+      {pattern: './src/**/*.css', included: false, watched: true, served: true},
+    ],
+    preprocessors: {'./src/test.ts': ['webpack', 'sourcemap']},
+    webpack: {
+      resolve: {extensions: ['.ts', '.js']},
+      module: {
+        rules: [
+          // {test: /\.ts$/, loader: 'tslint-loader', exclude: /node_modules/,
+          // enforce: 'pre'},
+          {
+            test: /\.ts$/,
+            loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
+            exclude: /node_modules/,
+            include: /src/
+          },
+          {
+            test: /\.(html|css)$/,
+            loader: 'raw-loader',
+            exclude: /\.async\.(html|css)$/
+          },
+          {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            loaders: ['raw-loader', 'sass-loader']
+          },
+          {
+            test: /src\/.+\.ts$/,
+            exclude: /(node_modules|\.spec\.ts$)/,
+            loader: 'istanbul-instrumenter-loader',
+            enforce: 'post'
+          }
+        ]
+      },
+      plugins: [
+        new webpack.SourceMapDevToolPlugin(
+            {filename: null, test: /\.(ts|js)($|\?)/i}),
+        // new webpack.LoaderOptionsPlugin({options: {tslint: {emitErrors:
+        // config.singleRun, failOnHint: false}}}),
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)@angular/, path.join(__dirname, 'src'))
+      ].concat((config.singleRun ? [new webpack.NoEmitOnErrorsPlugin()] : []))
+    },
+    mime: {'text/x-typescript': ['ts', 'tsx']},
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: !coverage ? ['text-summary'] : ['html', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    angularCli: {
-      environment: 'dev'
+    storyReporter: {
+      showSkipped: true,
+      showSkippedSummary: false  // default: false
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['story', 'coverage-istanbul'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_ERROR,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma
+      // exits without killing phantom)
+      exitOnResourceError: true
+    },
+    browserConsoleLogOptions: {terminal: true, level: 'error'}
   });
 };
